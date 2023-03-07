e310ce531357491e5f6979ae5322f1085f2de1b3
itsbilyatt <106489353+itsbilyatt@users.noreply.github.com>
2023-03-07 07:40:29 +0530

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup
+
+
+# read the contents of your README file
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
+setup(
+    name='hyperknee_finder',
+    version='0.0.1',
+    description='It's about a tool for optimizing two inter-dependent parameters.',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/itsbilyatt/hyperknee_finder',
+    author='Vincenzo Lavorini',
+    author_email='vincenzo.lavorini@protonmail.ch',
+    license='BSD 2-clause',
+    packages=['hyperkneefinder'],
+    install_requires=[
+                      'numpy',
+                      'matplotlib',
+                      'sklearn'
+                      ],
+
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: POSIX :: Linux',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+    ],
+)
